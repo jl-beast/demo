a3e84bb3bb24b7a223fee755d4121768cf25c2af
Jackson Levitt <jackson.levitt@beast-code.com>
2023-09-08 13:59:57 -0500

    Demo Commit

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿
+string answer = Console.ReadLine();
+
+if (answer == "red")
+{
+    Console.BackgroundColor = ConsoleColor.Red;
+    Console.WriteLine(answer);
+}
+
+if (answer == "green")
+{
+    Console.BackgroundColor = ConsoleColor.Green;
+    Console.WriteLine(answer);
+}
